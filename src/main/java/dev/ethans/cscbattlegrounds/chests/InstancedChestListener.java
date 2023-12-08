1750c285f8da0917623e11d93ed9466241736d88
Ethan Smith <ethan.smith1845@gmail.com>
2023-12-08 17:15:31 -0600

    Make sure a chest is actually an instanced chest before creating unique inventories.

--- src/main/java/dev/ethans/cscbattlegrounds/chests/InstancedChestListener.java
+++ src/main/java/dev/ethans/cscbattlegrounds/chests/InstancedChestListener.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -18,17 +19,20 @@ public class InstancedChestListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onChestOpen(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+
         Player player = event.getPlayer();
 
         if (event.getClickedBlock() == null) return;
+
         Block block = event.getClickedBlock();
+
         if (block.getType() != Material.CHEST) return;
+        if (!block.hasMetadata("chest-id")) return;
 
         event.setCancelled(true);
-
         Inventory inventory = plugin.getServer().createInventory(null, 27, Component.text("Chest"));
         new InstancedChest(inventory);
-
         player.openInventory(inventory);
     }
 }
